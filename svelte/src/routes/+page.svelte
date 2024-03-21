31ee37f99b21baf0f8551a789c8d85cdd207d4f9
Madeleine Lorås <madeleine.loras@gmail.com>
2024-03-21 14:44:42 +0100

    setup

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  async function generateActivity() {
+    console.log("hello world");
+    const response = await fetch("https://www.boredapi.com/api/activity");
+    var data = response.json();
+  }
+
+  const fetchActivity = (async () => {
+    const response = await fetch("https://www.boredapi.com/api/activity");
+    return await response.json();
+  })();
+</script>
+
+<div class="bored-header">
+  <h1>Are you bored?</h1>
+  <button on:click={generateActivity}>Click for activity</button>
+
+  <h2 id="activity">Activity</h2>
+  <div class="activity-card">
+    <div class="activity-info">
+      <p>Type:</p>
+      <p id="type"></p>
+    </div>
+    <div class="activity-info">
+      <p>Accessability:</p>
+      <p id="accessability"></p>
+    </div>
+    <div class="activity-info">
+      <p>Costlyness:</p>
+      <p id="price"></p>
+    </div>
+    <div class="activity-info">
+      <p>Participants:</p>
+      <p id="participants"></p>
+    </div>
+  </div>
+</div>
