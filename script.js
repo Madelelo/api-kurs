52d5a58abb4987c08d89f5d18f2ba05e9139a0b7
Madeleine Lorås <madeleine.loras@gmail.com>
2024-04-02 16:20:37 +0200

    setup server

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 console.log("hello");
 
-const url = "http://www.boredapi.com/api/activity";
+const URL = "http://localhost:3000/api/games/";
 
-async function generateActivity() {
-  const res = await fetch(url);
+async function generateGameCards() {
+  const res = await fetch(URL);
   const data = await res.json();
-
   console.log(data);
 
-  document.getElementById("activity").innerHTML = data.activity;
-  document.getElementById("type").innerHTML = data.type;
-  document.getElementById("accessability").innerHTML =
-    data.accessibility * 10 + "/10";
-  document.getElementById("price").innerHTML = data.price * 10 + "/10";
-  document.getElementById("participants").innerHTML = data.participants;
+  data.map((game) => {
+    document.getElementById("game-name").innerHTML = game.name;
+    document.getElementById("game-type").innerHTML = game.type;
+    document.getElementById("game-year").innerHTML = game.year;
+    document.getElementById("game-console").innerHTML = game.console;
+    document.getElementById("game-company").innerHTML = game.company;
+  });
 }
 
 function submitChoice() {
   console.log("yolo");
+}
+
+generateGameCards();
+
+function createUser() {
+  fetch("http://localhost:3000/api/newuser", {
+    method: "POST",
+    body: JSON.stringify({
+      id: "4",
+      name: "Faizan",
+      username: "faizan",
+      email: "mmm@mmm.no",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // title = document.getElementById("title");
+      // body = document.getElementById("bd");
+      // title.innerHTML = data.title;
+      // body.innerHTML = data.body;
+    })
+    .catch((error) => console.error("Error:", error));
 }
