7edbd53a81fec08aac4f5c82aa489c0c73a709b1
Madeleine Lorås <madeleine.loras@gmail.com>
2024-04-03 16:37:16 +0200

    final version

---
+++ bored-app/script.js
@@ -0,0 +1,26 @@
+const APIURL = "http://www.boredapi.com/api/activity";
+
+async function generateActivity() {
+  const response = await fetch(APIURL); // Henter fra APIURL
+  const data = await response.json(); // Gjør om responsen til JSON
+  console.log(response, data);
+
+  writeToHTML(data);
+}
+
+async function generateActivityByType(type) {
+  const response = await fetch(APIURL + "?type=" + type); // Henter fra APIURL
+  const data = await response.json(); // Gjør om responsen til JSON
+  //console.log(response, data);
+
+  writeToHTML(data);
+}
+
+function writeToHTML(data) {
+  document.getElementById("activity").innerHTML = data.activity;
+  document.getElementById("type").innerHTML = data.type;
+  document.getElementById("accessability").innerHTML =
+    data.accessibility * 10 + "/10";
+  document.getElementById("price").innerHTML = data.price * 10 + "/10";
+  document.getElementById("participants").innerHTML = data.participants;
+}
